73be45f00486f4f880311868166eae77c07571fa
ekopro <ekoprok@voliacable.com>
2017-11-07 10:03:47 +0200

    first
    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# WooTemplate-Learning
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+module.exports = {
+  watch: true,
+  entry: {
+      main: [ './dev/src/js/index.js','./dev/src/sass/style.sass'] // need include entry for sass file
+    },
+    output: {
+    path: path.resolve(__dirname,'dev/dist/js'),
+    filename: 'bundle.js'
+ },
+ devtool: 'source-map',
+ plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: '../index.html',
+            template: './dev/src/index.pug'
+        }),
+        new ExtractTextPlugin('../css/style.css')
+   ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loaders: ['pug-loader']
+      },
+      {
+          test: /\.(png|svg|jpg|gif)$/,
+          loaders: 'file-loader',
+          exclude: /fonts/,
+          options: {
+            outputPath: 'img/',
+            name: '[name].[ext]',
+            publicPath: './'
+          }
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          loader: 'file-loader',
+            options: {
+            outputPath: '../fonts/'
+
+          }
+        },
+      {
+        test: /\.s[ac]ss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','sass-loader']
+        })
+      }
+    ]
+  }
+}
